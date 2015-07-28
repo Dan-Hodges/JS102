2e65eda762436a3876b1ae0885f7f75e51193c93
Dan-Hodges <dhdgs690@gmail.com>
2015-07-28 10:18:28 -0500

    created JS files to sort array on click

---
+++ main.js
@@ -0,0 +1,33 @@
+function sorter (myArray) {
+return myArray.sort();
+}
+function largerThan (myArray, newArray) {
+  for (var i = 0; i <= myArray.length; i++) {
+    if (myArray[i] > 24) {
+      var newItem = myArray[i]; 
+      newArray.push(newItem);
+    }
+  }
+}
+
+$(document).ready(function() {
+  $('#submit').click(function() {
+    arrayLarge = [];
+    arrayOriginal = [];
+    newArray = [];
+    var myString;
+    var myOutput;
+    console.log("at least the click works");
+    myString = $('#input').val(); 
+    console.log("my string is" + myString);
+    arrayOriginal = myString.split(',');
+    console.log("my array original is " + arrayOriginal);
+    largerThan(arrayOriginal, newArray);
+    console.log("The new array is:" + newArray);
+    for (var i = 0; i < newArray.length; i++) {
+      myOutput += "<div class='number'>" + newArray[i] + "</div>"
+    }
+    console.log("here's my output:" + myOutput);
+    $('#content').html(myOutput);
+  });
+});  
